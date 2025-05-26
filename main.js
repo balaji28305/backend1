32f35c5e39aac2a13747a3ac6c04746c63bb684b
balaji28305 <balaji28305@gmail.com>
2025-05-27 02:55:01 +0530

    route structure

--- main.js
+++ main.js
@@ -5,7 +5,27 @@ const port =6969;
 app.get('/',(req,res)=>{
     res.json({msg:"Hello World!"});
 });
+//crud functionality of movies
 
+//R-reading data
+app.get('/movies',()=>{
+
+})
+
+//C-creating data
+app.post('/movies',()=>{
+
+})
+
+//U-update data
+app.put('/movies',()=>{
+
+})
+
+//D-delete data
+app.delete('/movies/:id',()=>{
+    
+})
 
 app.listen(port,()=>{
     console.log(`the server is running on port http://localhost:${port}`);
